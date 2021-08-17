da00c1472a6ef90dd8fcaad2e6f41dce8a77ae09
Marc Majcher <majcher@gmail.com>
2021-08-17 17:27:07 -0500

    lesson

---
+++ src/components/ToyCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ToyCard({ toy, handleDelete, handleClickLikes }) {
+  const { id, image, name, likes } = toy;
+
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button onClick={() => handleClickLikes(toy)} className="like-btn">Like {'<3'}</button>
+      <button onClick={() => handleDelete(id)} className="del-btn">Donate to GoodWill</button>
+    </div>
+  );
+}
+
+export default ToyCard;
